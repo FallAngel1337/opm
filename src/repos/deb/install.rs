07069758d2681dd98457dc796bac6360e664ff59
FallAngel1337 <fallangel@protonmail.com>
2021-12-06 10:25:34 -0300

    Trying to lookup in the cache

--- src/repos/deb/install.rs
+++ src/repos/deb/install.rs
@@ -3,28 +3,32 @@
 /// 
 
 use crate::repos::errors::InstallError;
-use crate::repos::database::SQLite;
 use crate::repos::config::Config;
+use super::package::ControlFile;
 use super::extract;
 use super::download;
+use std::fs;
+
+fn cache_lookup(config: &Config, _name: &str) {
+    for entry in fs::read_dir(&config.cache).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        let contents = fs::read_to_string(path).unwrap();
+        println!("Contents: {}", contents);
+    };
+}
 
 pub fn install(config: &mut Config, name: &str) -> Result<(), InstallError> {
-    let mut sqlite = SQLite::new(&mut config.pkgs);
-    sqlite.init()?;
-    sqlite.create_tables()?;
-    
     if name.ends_with(".deb") {
         let _pkg = extract::extract(name, &config.tmp)?;
-        // sqlite.register(pkg)?;
         println!("Extracting ...");
-
     } else {
         download::download(config, name)?;
-
     }
+
+    cache_lookup(&config, name);
     println!("Installing {}", name);
 
-        
-    
     Ok(())
 }
