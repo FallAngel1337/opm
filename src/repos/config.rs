45b4e4fc2b1b5d59c2be0584ad271e81d1046a31
FallAngel1337 <fallangel@protonmail.com>
2021-12-16 07:45:40 -0300

    Improving config & tabs over spaces

--- src/repos/config.rs
+++ src/repos/config.rs
@@ -3,82 +3,104 @@ use std::io::{Error, ErrorKind};
 use std::env;
 use std::fs;
 
-use super::database::SQLite;
+use super::{database::SQLite, errors::ConfigError};
+use super::utils::PackageFormat;
 
 #[derive(Debug)]
 pub struct Config {
-    pub root: PathBuf,
-    pub cache: PathBuf,
-    pub db: PathBuf,
-    pub rls: PathBuf,
-    pub tmp: PathBuf,
+	pub root: PathBuf,
+	pub cache: PathBuf,
+	pub db: PathBuf,
+	pub rls: PathBuf,
+	pub tmp: PathBuf,
 
-    pub sqlite: Option<SQLite>,
+	pub sqlite: Option<SQLite>,
 }
 
 #[allow(deprecated)]
 impl Config {
-    pub fn new() -> Result<Self, Error> {
-        let home = env::home_dir().unwrap()
-            .into_os_string().into_string().unwrap();
-         
-        Ok(
-            Self {
-                root: PathBuf::from(format!("{}/.opm/", home)),
-                cache: PathBuf::from(format!("{}/.opm/cache/pkg_cache", home)),
-                db: PathBuf::from(format!("{}/.opm/db/pkgs.db", home)),
-                rls: PathBuf::from(format!("{}/.opm/cache/rls", home)),
-                tmp: PathBuf::from(format!("{}/.opm/tmp", home)),
-                sqlite: None
-            }
-        )
-    }
-    
-    pub fn close_db(&self) {
-        if let Some(conn) = self.sqlite.as_ref() {
-            conn.close()
-        }
-    }
+	pub fn new() -> Result<Self, ConfigError> {
+		let home = env::home_dir().unwrap()
+			.into_os_string().into_string().unwrap();
+		let pkg_fmt_name;
+			
+		if let Some(pkg_fmt) = PackageFormat::get_format() {
+			pkg_fmt_name = match pkg_fmt {
+				PackageFormat::Deb => {
+					"deb"
+				},
+				PackageFormat::Rpm => {
+					println!("It's a RHEL(-based) distro");
+					"rpm"
+				},
+				PackageFormat::Other => {
+					println!("Actually we do not have support for you distro!");
+					"oth"
+				},
+			};
+		} else {
+			eprintln!("Consider define `PKG_FMT` environment variable!");
+			std::process::exit(1);
+		}
 
-    pub fn setup_db(&mut self) -> rusqlite::Result<()> {
-        if self.sqlite.as_ref().is_none() {
-            self.sqlite = Some(
-                SQLite::new(&self.db)?
-            );
-        }
-        Ok(())
-    }
+		let opm_root = format!("{}/.opm/{}", home, pkg_fmt_name);
 
-    pub fn setup(&mut self) -> Result<(), Error> {
-        let path = std::path::Path::new(&self.db);
-        let prefix = path.parent().unwrap();
-        fs::create_dir_all(prefix)?;
+		Ok(
+			Self {
+				root: PathBuf::from(&opm_root),
+				cache: PathBuf::from(format!("{}/cache/pkg_cache", opm_root)),
+				db: PathBuf::from(format!("{}/db/pkgs.db", opm_root)),
+				rls: PathBuf::from(format!("{}/cache/rls", opm_root)),
+				tmp: PathBuf::from(format!("{}/tmp", opm_root)),
+				sqlite: None
+			}
+		)
+	}
 
-        match fs::create_dir_all(&self.cache) {
-            Ok(_) => (),
-            Err(e) => match e.kind() {
-                ErrorKind::AlreadyExists => (),
-                _ => panic!("Some error occurred {}", e)
-            }
-        }
+	pub fn close_db(&self) {
+		if let Some(conn) = self.sqlite.as_ref() {
+			conn.close()
+		}
+	}
 
-        match fs::create_dir_all(&self.rls) {
-            Ok(_) => (),
-            Err(e) => match e.kind() {
-                ErrorKind::AlreadyExists => (),
-                _ => panic!("Some error occurred {}", e)
-            }
-        }
-        
-        match fs::create_dir_all(&self.tmp) {
-            Ok(_) => (),
-            Err(e) => match e.kind() {
-                ErrorKind::AlreadyExists => (),
-                _ => panic!("Some error occurred {}", e)
-            }
-        }
+	pub fn setup_db(&mut self) -> rusqlite::Result<()> {
+		if self.sqlite.as_ref().is_none() {
+			self.sqlite = Some(
+				SQLite::new(&self.db)?
+			);
+		}
+		Ok(())
+	}
 
+	pub fn setup(&mut self) -> Result<(), Error> {
+		let path = std::path::Path::new(&self.db);
+		let prefix = path.parent().unwrap();
+		fs::create_dir_all(prefix)?;
 
-        Ok(())
-    }
+		match fs::create_dir_all(&self.cache) {
+			Ok(_) => (),
+			Err(e) => match e.kind() {
+			ErrorKind::AlreadyExists => (),
+				_ => panic!("Some error occurred {}", e)
+			}
+		}
+
+		match fs::create_dir_all(&self.rls) {
+			Ok(_) => (),
+			Err(e) => match e.kind() {
+			ErrorKind::AlreadyExists => (),
+				_ => panic!("Some error occurred {}", e)
+			}
+		}
+
+		match fs::create_dir_all(&self.tmp) {
+			Ok(_) => (),
+			Err(e) => match e.kind() {
+			ErrorKind::AlreadyExists => (),
+				_ => panic!("Some error occurred {}", e)
+			}
+		}
+
+		Ok(())
+	}
 }
