2bd1d8b698e2b6bfd0f26edf9e7586fe58b430d3
FallAngel1337 <fallangel@protonmail.com>
2021-12-30 13:56:14 +0000

    Removed useless stuff and fixed the unpacking package format to work with both xz and gz

--- src/repos/deb/update.rs
+++ src/repos/deb/update.rs
@@ -1,88 +1,30 @@
-use anyhow::Result;
+use anyhow::{Result, Context};
 use xz2::read::XzDecoder;
+use flate2::read::GzDecoder;
 use std::{
     io::{ErrorKind, prelude::*},
     fs::{self, File},
     path::Path,
-    str, collections::HashMap
+    str
 };
 use reqwest;
 use futures::future;
 use super::sources::DebianSource;
 use crate::repos::config::Config;
 
-#[derive(Debug)]
-struct Release {
-    origin: String,
-    label: String,
-    suite: String,
-    version: String,
-    codename: String,
-    date: String,
-    architectures: Vec<String>,
-    components: Vec<String>,
-    description: String,
-    md5sum: Vec<String>
-}
+fn unpack(filename: &str, data: &[u8], bytes: &mut Vec<u8>) -> Result<()> {
 
-impl Release {
-    fn new(file: &Path) -> Result<Self> {
-        let mut map: HashMap<Option<String>, Option<String>> = HashMap::new();
-        let contents = std::fs::read_to_string(file)?;
-
-        for line in contents.lines() {
-            let line = line.trim();
-            let values = line.splitn(2, ':').map(|line| line.to_owned()).collect::<Vec<_>>();
-            map.insert(
-                values.get(0).map(|v| v.to_owned()),
-                values.get(1).map(|v| v.to_owned())
-            );
-        }
-
-        Ok(
-            Self {
-                origin: Self::try_get(&map, "Origin")?,
-                label: Self::try_get(&map, "Label")?,
-                suite: Self::try_get(&map, "Suite")?,
-                version: Self::try_get(&map, "Version")?,
-                codename: Self::try_get(&map, "Codename")?,
-                description: Self::try_get(&map, "Description")?,
-                date: Self::try_get(&map, "Date")?,
-                architectures: Self::split_deps(Some(&Self::try_get(&map, "Architectures").unwrap_or_default())).unwrap(),
-                components: Self::split_deps(Some(&Self::try_get(&map, "Components").unwrap_or_default())).unwrap(),
-                md5sum: Self::split_deps(Some(&Self::try_get(&map, "Components").unwrap_or_default())).unwrap(),
-            }
-        )
+    if filename.ends_with(".tar.gz") {
+        let mut tar = GzDecoder::new(data);
+        tar.read_to_end(bytes)
+        .with_context(|| format!("Could not unpack {} archive", filename))?;
+    } else if filename.ends_with(".tar.xz") {
+        let mut tar = XzDecoder::new(data);
+        tar.read_to_end(bytes)
+        .with_context(|| format!("Could not unpack {} archive", filename))?;
     }
 
-    fn try_get(hashmap: &HashMap<Option<String>, Option<String>>, field: &str) -> Result<String> {
-        let value = hashmap.get(&Some(field.to_owned()));
-        if let Some(v) = value {
-            if let Some(v) = v {
-                Ok (v.trim().to_owned())
-            } else {
-                anyhow::bail!(format!("Unknown error trying to get \"{}\" field", field));
-            }
-        } else {
-            anyhow::bail!(format!("Invalid debain's control file! Missing \"{}\" field", field));
-        }
-    }
-
-    fn split_deps(dependencies: Option<&String>) -> Option<Vec<String>> {
-        if let Some(val) = dependencies {
-            if !val.is_empty() {
-                let val = val
-                    .split(',')
-                    .map(|d| d.trim().to_owned())
-                    .collect::<Vec<_>>();
-                Some(val)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }
+    Ok(())
 }
 
 pub fn clear(config: &Config) -> Result<()> {
@@ -137,7 +79,6 @@ async fn update_cache(config: &Config, repos: &[DebianSource]) -> Result<()> {
             let url = str::replace(&url, "/", "_");
 
             let pkg = Path::new(&config.cache).join(format!("{}dists_{}_{}_binary-amd64_Packages", url, source.distribution, perm));
-            let rsl = format!("{}dists_{}_{}_binary-amd64_Packages", url, source.distribution, perm);
 
             let old_size = if let Ok(mut file) = File::open(&pkg) {
                 let mut old_data = Vec::new();
@@ -155,11 +96,8 @@ async fn update_cache(config: &Config, repos: &[DebianSource]) -> Result<()> {
 
                 println!("Hit {}: {} [{} kB]", i+1, pkgcache, content.len() / 1024);
                 
-                let mut data = XzDecoder::new(content.as_ref());
                 let mut bytes = Vec::new();
-                
-                data.read_to_end(&mut bytes).unwrap_or_default();
-                
+                unpack(pkgcache, content.as_ref(), &mut bytes).unwrap();
                 let mut bytes: &[u8] = bytes.as_ref();
                 
                 if bytes.len() != old_size {
