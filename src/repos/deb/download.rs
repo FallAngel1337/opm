adf785b5f479067643056f03e4fb3f1f1eae732b
FallAngel1337 <fallangel@protonmail.com>
2022-01-07 18:06:15 -0300

    Adding a better download messsage

--- src/repos/deb/download.rs
+++ src/repos/deb/download.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use bytesize::ByteSize;
 use std::{path::PathBuf, fs::File, io};
 
 use crate::repos::config::Config;
@@ -6,24 +7,26 @@ use super::{package::DebPackage, signatures};
 use reqwest;
 
 pub async fn download(config: &Config, pkg: DebPackage) -> Result<(PathBuf, String)> {
-    let pkg_name = pkg.control.package.clone();
-    println!("Downloading {} ...", pkg_name);
+    let control = pkg.control.clone();
+    let url = format!("http://{}", control.filename);    
+    let response = reqwest::get(&url).await?;
 
-    let response = reqwest::get(format!("http://{}", &pkg.control.filename)).await?;
-    
     let content = response.bytes().await?;
     let mut content: &[u8] = content.as_ref();
+
+    println!("Get: {} {} {} {} [{}]", url, control.architecture,
+    control.package, control.version, ByteSize(content.len() as u64));
 
     match signatures::verify_sig(&pkg, content) {
         Ok(()) => (),
         Err(e) => eprintln!("Failed to check package signature :: {}", e)
     }
 
-    let name = pkg.control.filename.split('/').last().unwrap().to_string();
+    let name = control.filename.split('/').last().unwrap().to_string();
     let fname = format!("{}/{}", config.tmp, name);
 
     let mut pkg = File::create(&fname)?;
     io::copy(&mut content, &mut pkg)?;
 
-    Ok((PathBuf::from(&fname), pkg_name))
+    Ok((PathBuf::from(&fname), control.package))
 }
