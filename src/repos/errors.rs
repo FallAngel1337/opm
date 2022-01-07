8f5eba04fdeb939f507d9276910e19e41132fc48
FallAngel1337 <fallangel@protonmail.com>
2022-01-07 18:34:08 -0300

    Changed the `BrokenPackage` message

--- src/repos/errors.rs
+++ src/repos/errors.rs
@@ -50,7 +50,7 @@ impl Display for InstallError {
             InstallError::NetworkingError { err } => write!(f, "Networking Error :: \"{}\"", err),
             InstallError::AlreadyInstalled => write!(f, "Package is already installed"),
             InstallError::Interrupted => write!(f, "Installation got interrupted by the user"),
-            InstallError::BrokenPackage(msg) => write!(f, "Perhaps {} is broken", msg),
+            InstallError::BrokenPackage(msg) => write!(f, "Perhaps {} is broken due some missing files", msg),
             InstallError::Breaks(pkg) => write!(f, "Package {} can break others", pkg),
             InstallError::NotFoundError(pkg) => write!(f, "Package {} was not found", pkg),
             InstallError::Error(msg) => write!(f, "Error during installation :: {}", msg)
