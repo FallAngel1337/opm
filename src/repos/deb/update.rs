a0677219268e52fdd93961287721b7de51ec1735
FallAngel1337 <fallangel@protonmail.com>
2022-01-14 17:41:08 -0300

    Adding a spinner to the update function

--- src/repos/deb/update.rs
+++ src/repos/deb/update.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle, HumanBytes};
 use xz2::read::XzDecoder;
 use flate2::read::GzDecoder;
 use std::{
@@ -8,7 +9,7 @@ use std::{
     str
 };
 use reqwest;
-use futures::future;
+use futures::{future, StreamExt};
 use super::sources::DebianSource;
 use crate::repos::config::Config;
 
@@ -56,82 +57,114 @@ pub fn clear(config: &Config) -> Result<()> {
 }
 
 pub async fn update(config: &mut Config, repos: &[DebianSource]) -> Result<()> {
-    update_releases(config, repos).await?;
-    update_cache(config, repos).await?;
+    let (mut cache, mut rls) = (vec![], vec![]);
+    let spinner_style = ProgressStyle::default_spinner()
+        .template("{spinner} {prefix}");
 
-    Ok(())
-}
-
-async fn update_cache(config: &Config, repos: &[DebianSource]) -> Result<()> {
-    let mut tasks = vec![];
+    let mp = MultiProgress::new();
     for (i, source) in repos.iter().enumerate() {
         for perm in source.components.iter() {
-            // Binary packages ONLY for now
-            let pkgcache = format!("{}dists/{}/{}/binary-amd64/Packages.xz", source.url, source.distribution, perm);
-            let response = match reqwest::get(&pkgcache).await {
-                Ok(r) => Some(r),
-                Err(_) => {
-                    let pkgcache = format!("{}dists/{}/{}/binary-amd64/Packages.gz", source.url, source.distribution, perm);
-                    match reqwest::get(&pkgcache).await {
-                        Ok(r) => Some(r),
-                        Err(e) => {
-                            eprintln!("Could not get the package at {} due {}", pkgcache, e);
-                            None
-                        }
-                    }
-                }
-            };
+            let cache_bar = mp.add(ProgressBar::new(0));
+            cache_bar.set_style(spinner_style.clone());
 
-            let url = str::replace(&source.url, "http://", "");
-            let url = str::replace(&url, "/", "_");
-
-            let pkg = Path::new(&config.cache).join(format!("{}dists_{}_{}_binary-amd64_Packages", url, source.distribution, perm));
+            let rls_bar = mp.add(ProgressBar::new(0));
+            rls_bar.set_style(spinner_style.clone());
             
-            tasks.push(tokio::spawn(async move {
-                if let Some(res) = response {
-                    let content = res.bytes().await.unwrap();
-                    println!("Hit {}: {} [{} kB]", i+1, pkgcache, content.len() / 1024);
-                    
-                    let mut bytes = Vec::new();
-                    unpack(&pkgcache, content.as_ref(), &mut bytes);
-                    let mut bytes: &[u8] = bytes.as_ref();
-                    if !bytes.is_empty() {
-                        let mut pkg = tokio::fs::File::create(pkg).await.unwrap();
-                        tokio::io::copy(&mut bytes, &mut pkg).await.unwrap();
-                    }
-                }
-
-            }));
-        };
-    }
-
-    future::join_all(tasks).await;
-    Ok(())
-}
-
-async fn update_releases(config: &Config, repos: &[DebianSource]) -> Result<()> {
-    let mut tasks = vec![];
-    
-    for (i, source) in repos.iter().enumerate() {
-        for perm in source.components.iter() {
-            let release_file = format!("{}dists/{}/InRelease", source.url, source.distribution);
-            
-            let url = str::replace(&source.url, "http://", "");
-            let url = str::replace(&url, "/", "_");
-            
-            let rls = Path::new(&config.rls).join(format!("{}dists_{}_{}_binary-amd64_InRelease", url, source.distribution, perm));
-            
-            tasks.push(tokio::spawn(async move {
-                let response = reqwest::get(&release_file).await.unwrap();
-                let content =  response.text().await.unwrap();
-                println!("Hit {}: {} [{} kB]", i+1, release_file, content.len() / 1024);
-                
-                let mut dest = tokio::fs::File::create(rls).await.unwrap();
-                tokio::io::copy(&mut content.as_bytes(), &mut dest).await.unwrap();
-            }));
+            cache.push(update_cache(config, &source.url, &source.distribution, perm, cache_bar, i));
+            rls.push(update_releases(config, &source.url, &source.distribution, perm, rls_bar, i));
         }
     }
+    let handle = tokio::task::spawn_blocking(move || mp.join().unwrap());
 
-    future::join_all(tasks).await;
+    future::join_all(rls).await;
+    future::join_all(cache).await;
+    
+    handle.await?;
+
+    Ok(())
+}
+
+async fn update_cache(config: &Config, url: &str, dist: &str, perm: &str, pb: ProgressBar, counter: usize) -> Result<()> {
+    // Binary packages ONLY for now
+    let pkgcache = format!("{}dists/{}/{}/binary-amd64/Packages.xz", url, dist, perm);
+    let response = match reqwest::get(&pkgcache).await {
+        Ok(r) => Some(r),
+        Err(_) => {
+            let pkgcache = format!("{}dists/{}/{}/binary-amd64/Packages.gz", url, dist, perm);
+            match reqwest::get(&pkgcache).await {
+                Ok(r) => Some(r),
+                Err(e) => {
+                    eprintln!("Could not get the package at {} due {}", pkgcache, e);
+                    None
+                }
+            }
+        }
+    };
+
+    let url = str::replace(url, "http://", "");
+    let url = str::replace(&url, "/", "_");    
+    
+    if let Some(response) = response {
+        let size = response.content_length().unwrap_or_default();
+        pb.set_length(size);
+        pb.set_prefix(format!("{}: {} [{}]", counter+1, pkgcache, HumanBytes(size)));
+
+        let (mut stream, mut downloaded) = (response.bytes_stream(), 0u64);
+        
+        let mut content = Vec::with_capacity(size as usize);
+        let pkg = Path::new(&config.cache).join(format!("{}dists_{}_{}_binary-amd64_Packages", url, dist, perm));
+
+        while let Some(item) = stream.next().await {
+            let chunk = item?;
+            Write::write(&mut content, &chunk)?;
+            let progress = std::cmp::min(downloaded + chunk.len() as u64, size);
+            downloaded = progress;
+            pb.set_position(progress);
+        }
+        
+        let mut bytes = Vec::new();
+        unpack(&pkgcache, content.as_ref(), &mut bytes);
+        let mut bytes: &[u8] = bytes.as_ref();
+        if !bytes.is_empty() {
+            let mut pkg = tokio::fs::File::create(pkg).await.unwrap();
+            tokio::io::copy(&mut bytes, &mut pkg).await.unwrap();
+        }
+
+        pb.finish_and_clear()
+    }
+    
+    Ok(())
+}
+
+async fn update_releases(config: &Config, url: &str, dist: &str, perm: &str, pb: ProgressBar, counter: usize) -> Result<()> {
+
+    let release_file = format!("{}dists/{}/InRelease", url, dist);
+
+    let url = str::replace(url, "http://", "");
+    let url = str::replace(&url, "/", "_");
+    
+    let response = reqwest::get(&release_file).await?;
+    let size = response.content_length().unwrap_or_default();
+    pb.set_length(size);
+    pb.set_prefix(format!("{}: {} [{}]", counter+1, release_file, HumanBytes(size)));
+    
+    let (mut stream, mut downloaded) = (response.bytes_stream(), 0u64);
+
+    let mut content = Vec::with_capacity(size as usize);
+    let rls = Path::new(&config.rls).join(format!("{}dists_{}_{}_binary-amd64_InRelease", url, dist, perm));
+
+    while let Some(item) = stream.next().await {
+        let chunk = item?;
+        Write::write(&mut content, &chunk)?;
+        let progress = std::cmp::min(downloaded + chunk.len() as u64, size);
+        downloaded = progress;
+        pb.set_position(progress);
+    }
+    
+    
+    let mut dest = tokio::fs::File::create(rls).await.unwrap();
+    tokio::io::copy(&mut content.as_ref(), &mut dest).await.unwrap();
+
+    pb.finish_and_clear();
     Ok(())
 }
