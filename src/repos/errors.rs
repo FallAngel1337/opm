185a44bf6f95c8478d6d5870b242eeeb7e27c1ab
FallAngel1337 <fallangel@protonmail.com>
2022-01-27 07:10:27 -0300

    Making better errors

--- src/repos/errors.rs
+++ src/repos/errors.rs
@@ -1,40 +1,37 @@
 use anyhow::Error;
 use std::fmt::{self, Display};
 
-// TODO: Make better erros and create more
 #[derive(Debug)]
 pub enum InstallError {
-    InvalidPackage { err: Error },
-    NetworkingError { err: Error },
-    AlreadyInstalled,
-    Interrupted,
-    BrokenPackage(String),
-    Breaks(String),
-    NotFoundError(String),
-    Error(String)
+    InvalidPackage   { pkg: String, err: Option<Error> },
+    BrokenPackage    { pkg: String, err: Option<Error> },
+    UnexError        { msg: String, err: Option<Error> },
+    AlreadyInstalled ( String ),
+    NotFoundError    ( String ),
+    Breaks           ( String ),
+    NetworkingError  { err: Error },
+    UnexInterrupt    { err: Error },
+    UserInterrupt,
 }
 
 #[derive(Debug)]
 pub enum SignatureError {
-    MD5(String, String),
-    SHA1(String, String),
-    SHA256(String, String),
-    SHA512(String, String)
+    MD5    { rs: String, ex: String },
+    SHA1   { rs: String, ex: String },
+    SHA256 { rs: String, ex: String },
+    SHA512 { rs: String, ex: String },
 }
 
 #[derive(Debug)]
-pub struct SetupError {
-    pub msg: String
-}
-
-#[derive(Debug)]
-pub struct  ConfigError {
-    pub msg: String
+pub struct ConfigError {
+    pub msg: String,
+    pub err: Option<Error>,
 }
 
 #[derive(Debug)]
 pub struct CacheError {
-    pub msg: String
+    pub msg: String,
+    pub err: Option<Error>,
 }
 
 impl std::error::Error for InstallError {
@@ -46,14 +43,15 @@ impl std::error::Error for InstallError {
 impl Display for InstallError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            InstallError::InvalidPackage { err } => write!(f, "Invalid Package :: \"{}\"", err),
-            InstallError::NetworkingError { err } => write!(f, "Networking Error :: \"{}\"", err),
-            InstallError::AlreadyInstalled => write!(f, "Package is already installed"),
-            InstallError::Interrupted => write!(f, "Installation got interrupted by the user"),
-            InstallError::BrokenPackage(msg) => write!(f, "Perhaps {} is broken due some missing files", msg),
-            InstallError::Breaks(pkg) => write!(f, "Package {} can break others", pkg),
-            InstallError::NotFoundError(pkg) => write!(f, "Package {} was not found", pkg),
-            InstallError::Error(msg) => write!(f, "Error during installation :: {}", msg)
+            InstallError::InvalidPackage { pkg, err } => write!(f, "{:?} is a invalid package :: error {:?}", pkg, err),
+            InstallError::BrokenPackage { pkg, err } => write!(f, "Perhaps {:?} is broken due some missing files :: error {:?}", pkg, err),
+            InstallError::AlreadyInstalled ( pkg ) => write!(f, "{:?} is already installed", pkg),
+            InstallError::UnexError { msg, err }  => write!(f, "Unexpected Error {:?} :: {:?}", msg, err),
+            InstallError::NotFoundError ( pkg ) => write!(f, "Package {:?} was not found in cache", pkg),
+            InstallError::Breaks ( pkg ) => write!(f, "Package {:?} can break others", pkg),
+            InstallError::NetworkingError { err } => write!(f, "Networking Error :: {:?}", err),
+            InstallError::UserInterrupt => write!(f, "Installation was interrupted by the user"),
+            InstallError::UnexInterrupt { err } => write!(f, "Installation was unexpected interrupted :: error {:?}", err),
         }
     }
 }
@@ -61,52 +59,22 @@ impl Display for InstallError {
 impl Display for SignatureError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            SignatureError::MD5(rs, ex) => write!(f, "Mismatched MD5 Hash got {} expected {}", rs, ex),
-            SignatureError::SHA1(rs, ex) => write!(f, "Mismatched SHA1 Hash got {} expected {}", rs, ex),
-            SignatureError::SHA256(rs, ex)=> write!(f, "Mismatched SHA256 Hash got {} expected {}", rs, ex),
-            SignatureError::SHA512(rs, ex) => write!(f, "Mismatched SHA512 Hash got {} expected {}", rs, ex),
+            SignatureError::MD5 { rs, ex } => write!(f, "Mismatched MD5 Hash got {:?} expected {:?}", rs, ex),
+            SignatureError::SHA1 { rs, ex } => write!(f, "Mismatched SHA1 Hash got {:?} expected {:?}", rs, ex),
+            SignatureError::SHA256 { rs, ex } => write!(f, "Mismatched SHA256 Hash got {:?} expected {:?}", rs, ex),
+            SignatureError::SHA512 { rs, ex } => write!(f, "Mismatched SHA512 Hash got {:?} expected {}", rs, ex),
         }
-    }
-}
-
-impl std::error::Error for SignatureError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(self)
-    }
-}
-
-impl std::error::Error for ConfigError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(self)
     }
 }
 
 impl Display for ConfigError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "ConfigError :: {}", self.msg)
-    }
-}
-
-impl std::error::Error for SetupError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(self)
-    }
-}
-
-impl Display for SetupError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "SetupError :: {}", self.msg)
-    }
-}
-
-impl std::error::Error for CacheError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(self)
+        write!(f, "ConfigError {:?} :: {:?}", self.msg, self.err)
     }
 }
 
 impl Display for CacheError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "CacheError :: {}", self.msg)
+        write!(f, "CacheError {:?} :: {:?}", self.msg, self.err)
     }
 }
