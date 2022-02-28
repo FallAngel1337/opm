2e79d6d309d42b434aed4185f65c3f1d61027148
FallAngel1337 <fallangel@protonmail.com>
2022-02-28 12:06:10 -0300

    Moving the `user_input()` function inside the crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,20 @@ extern "C" {
     pub fn getuid() -> u32;
 }
 
+pub fn user_input(msg: &str) -> anyhow::Result<()> {
+    let mut answer = String::new();
+    print!("{}", msg);
+    std::io::Write::flush(&mut std::io::stdout())?;
+    std::io::stdin().read_line(&mut answer)?;
+
+    if answer.to_ascii_lowercase().trim().starts_with('y') {
+        Ok(())
+    } else {
+        eprintln!("Exiting installation process...");
+        anyhow::bail!(repos::errors::InstallError::UserInterrupt);
+    }
+}
+
 mod repos;
 
 pub use repos::{setup, roll_back};
